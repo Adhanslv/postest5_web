420dc3c8d6e833bd39f98a88bbe7fdf7dba20ee5
Adhanslv <asyhab2002@gmail.com>
2022-10-17 11:45:57 +0800

    Postest5

---
+++ POSTEST5/about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="icon" href="https://www.freepnglogos.com/uploads/honda-logo-png/honda-motorcycles-logo-wing-10.png">
+    <style>
+      /* UNTUK TAMPILAN */
+      .gambar {
+        width: 290px;
+        height: 111px;
+      }
+      .links{
+        text-decoration: none;
+      }
+      /* UNTUK TAMPILAN */
+    </style>
+    <title>About Store</title>
+  </head>
+  <body>
+    <table border="1px" style="width: 100%; height: 100%;" >
+      <tr>
+        <th colspan="6">
+          <h2>Biodata</h2>
+        </th>
+      </tr>
+      <tr>
+        <td colspan="5"> Nama<br><br>Nim<br><br>No.Telp<br><br>Praktikum Web</td>
+        <td colspan="5"> Asyhab Ramadhan<br><br>2009106145<br><br>081254424739<br><br><a href="https://github.com/Adhanslv/adhanslv.github.io/tree/main/POSTEST2  " class="links">Postest 4 Web</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
